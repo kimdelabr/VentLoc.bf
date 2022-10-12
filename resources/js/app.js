57c837aa923e089fa31ee4e98980408a8db54c27
Arnaud KIMA <kimaarnaud1@gmail.com>
2022-10-12 00:20:25 +0000

    importation des dependances js et css et test avec la starter Page de admin-lte

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 require('./bootstrap');
+
+import "admin-lte/plugins/jquery/jquery.min.js";
+import "admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js";
+import "admin-lte/dist/js/adminlte.min.js?v=3.2.0";
