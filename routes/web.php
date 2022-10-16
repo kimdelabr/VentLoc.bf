ef3075dff0bc7bdfe0c1057ba425a8226f4d9164
Abdoul Aziz Bonkoungou <abbonkoungou10gmail.com>
2022-10-16 13:29:09 +0000

    Ajout du template et quelques modificatons

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,45 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/pages/index');
 });
 
+Route::get('/admin', function () {
+    return view('/admin');
+});
 
+Route::get('/home-02', function () {
+    return view('/pages/home-02');
+});
+
+Route::get('/home-03', function () {
+    return view('/pages/home-03');
+});
+
+Route::get('/contact', function () {
+    return view('/pages/contact');
+});
+
+Route::get('/product-detail', function () {
+    return view('/pages/product-detail');
+});
+
+Route::get('/product', function () {
+    return view('/pages/product');
+});
+
+Route::get('/shoping-cart', function () {
+    return view('/pages/shoping-cart');
+});
+
+Route::get('/blog', function () {
+    return view('/pages/blog');
+});
+
+Route::get('/blog-detail', function () {
+    return view('/pages/blog-detail');
+});
+
+Route::get('/about', function () {
+    return view('/pages/about');
+});
